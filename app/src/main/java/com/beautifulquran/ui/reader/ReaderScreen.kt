6d92af5147f48779358af7c873a0322813389c2d
Sammy Guergachi <sguergachi@gmail.com>
2026-07-04 23:27:00 -0400

    fix: add missing import for Modifier.height in ReaderScreen.kt

--- app/src/main/java/com/beautifulquran/ui/reader/ReaderScreen.kt
+++ app/src/main/java/com/beautifulquran/ui/reader/ReaderScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
